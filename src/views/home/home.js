54b9d5d7f9cbaf818672ebd76ea35b54f1e972af
vaishupadmor <vaishupadmor@gmail.com>
2024-08-18 16:59:53 +0530

    code-added

---
+++ src/views/home/home.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Navbar from "./../../component/navbar/navbar"
+import homeimg from "./../home/home-1.jpg"
+import Foodcard from "./../../component/foodcard/foodcard"
+import Footer from "./../../component/footer/footer"
+import "./home.css"
+import { PRODUCTS,MAIN_TITLE ,TAG_LINE ,THEME} from '../../config/data'
+
+
+function home() {
+  return (
+    <div>
+        <Navbar/>
+        
+        <h1 className='heading'>home</h1>
+        <img src={homeimg} className='images'/>
+        
+        
+    
+    <div >
+        <h1 className='food-title' style={{color:THEME.highlight , padding:0 , margin:0}}> {MAIN_TITLE}</h1>
+        <p className='food-tagline'>{TAG_LINE}</p>
+     
+        <div className='food-card-container'>  
+
+     { PRODUCTS.map((foodItem)=>{
+
+     return <Foodcard
+        imgUrl={foodItem.imgUrl}
+         title={foodItem.title}
+         description={foodItem.description}
+         
+         price={foodItem.price}
+    />
+  })
+}
+    </div>
+    </div>
+  
+
+
+
+        
+        <Footer/>
+    </div>
+  )
+}
+
+export default home
