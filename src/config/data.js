38fda98c9a362b86b933828972f5cf7eac25ced8
vaishupadmor <vaishupadmor@gmail.com>
2024-08-18 16:55:02 +0530

    code-added

---
+++ src/config/data.js
@@ -0,0 +1,55 @@
+
+const MAIN_TITLE = "Foods  heaven..."
+
+
+
+const TAG_LINE ="best food in town"
+
+
+
+const THEME = {
+    main:"#C0C78C",
+    highlight:"#A6B37D",
+    primary:"C0C78C",
+    secondary:"#B99470",
+    
+}
+const PRODUCTS =[
+    {
+      imgUrl:"https://th.bing.com/th/id/OIP.HMsEDd-6te9JWZ2A_EoosAHaE8?rs=1&pid=ImgDetMain",
+             title:"Allu Paratha"  ,
+             description:"fresh and tasty paratha",
+             isveg:true,
+             price:50
+    },
+    {
+      imgUrl:"https://i.pinimg.com/originals/14/55/1a/14551a1d3c7f79141e36788f09b57715.jpg",
+             title:"chicken biryani"  ,
+             description:"tasty and spicy",
+             isveg:false,
+             price:210
+    },
+    {
+      imgUrl:"https://th.bing.com/th/id/OIP.8UeIFPMYwIErE1ShRYB9QAHaEo?rs=1&pid=ImgDetMain",
+      title:"pizza"  ,
+       description:"tasty and yummy pizza with fresh dough",
+       isveg:true,
+       price:300
+    },
+    {
+      imgUrl:"https://www.recipemasters.in/wp-content/uploads/2015/06/dahi-bada2.jpeg",
+             title:"dahi wada"  ,
+             description:"mouth watering tasty dahi wada",
+             isveg:true
+    }]
+    
+
+    const PERSONS =[
+      {
+        imgUrl:"https://img.freepik.com/free-photo/smiling-cute-female-student-casual-shirt_176420-18233.jpg",
+             title:"prajakta"  ,
+             description:"BCS (bachlors of computer science) and PR manager",
+             
+      }
+    ]
+    export {PRODUCTS,MAIN_TITLE , TAG_LINE ,THEME }
